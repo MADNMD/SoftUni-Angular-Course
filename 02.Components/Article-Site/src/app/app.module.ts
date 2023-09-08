88e53c0038df45c91b6423d5875e85ee2dc17d1b
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-08 19:53:22 +0300

    Update app.module.ts

--- 02.Components/Article-Site/src/app/app.module.ts
+++ 02.Components/Article-Site/src/app/app.module.ts
@@ -1,16 +1,15 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArtilceComponent } from './artilce/artilce.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArtilceComponent,
     ArticleComponent,
     ArticlesComponent
   ],
