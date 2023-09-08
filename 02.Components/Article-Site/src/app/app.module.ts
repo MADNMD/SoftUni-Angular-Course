7b8875292c3f6ce319dfba1583e877d9bfc39661
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-08 19:46:10 +0300

    add article component

--- 02.Components/Article-Site/src/app/app.module.ts
+++ 02.Components/Article-Site/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtilceComponent } from './artilce/artilce.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtilceComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
