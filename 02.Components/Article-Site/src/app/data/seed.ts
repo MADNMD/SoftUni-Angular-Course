a5a910176464ca329ef7ae94bf9c77717bf6411b
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-08 19:49:14 +0300

    Update seed.ts

---
+++ 02.Components/Article-Site/src/app/data/seed.ts
@@ -0,0 +1,28 @@
+const data = [
+    {
+        title: 'Brown Bear',
+        description: 'The brown bear (Ursus arctos) is a large bear species found across Eurasia and North America. In North America, the populations of brown bears are called grizzly bears, while the subspecies that inhabits the Kodiak Islands of Alaska is known as the Kodiak bear. It is one of the largest living terrestrial members of the order Carnivora, rivaled in size only by its closest relative, the polar bear (Ursus maritimus), which is much less variable in size and slightly bigger on average. The brown bear\'s range includes parts of Russia, Central Asia, the Himalayas, China, Canada, the United States, Hokkaido, Scandinavia, Finland, the Balkans, the Picos de Europa and the Carpathian region (especially Romania), Iran, Anatolia, and the Caucasus. The brown bear is recognized as a national and state animal in several European countries.',
+        author: 'Mihail Donchev',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/330px-2010-kodiak-bear-1.jpg'
+    },
+    {
+        title: 'Tiger',
+        description: 'The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside. An apex predator, it primarily preys on ungulates, such as deer and wild boar. It is territorial and generally a solitary but social predator, requiring large contiguous areas of habitat to support its requirements for prey and rearing of its offspring. Tiger cubs stay with their mother for about two years and then become independent, leaving their mother\'s home range to establish their own.',
+        author: 'Nikola Donchev',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Walking_tiger_female.jpg/330px-Walking_tiger_female.jpg'
+    },
+    {
+        title: 'Cat',
+        description: 'he cat (Felis catus) is a domestic species of small carnivorous mammal.[1][2] It is the only domesticated species in the family Felidae and is commonly referred to as the domestic cat or house cat to distinguish it from the wild members of the family.[4] Cats are commonly kept as house pets but can also be farm cats or feral cats; the feral cat ranges freely and avoids human contact.[5] Domestic cats are valued by humans for companionship and their ability to kill small rodents. About 60 cat breeds are recognized by various cat registries.',
+        author: 'Vanya Nidenova',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/168px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg'
+    },
+    {
+        title: 'Pigeon',
+        description: 'The domestic pigeon (Columba livia domestica or Columba livia forma domestica)[2] is a pigeon subspecies that was derived from the rock dove (also called the rock pigeon). The rock pigeon is the world\'s oldest domesticated bird. Mesopotamian cuneiform tablets mention the domestication of pigeons more than 5,000 years ago, as do Egyptian hieroglyphics.[3] Research suggests that domestication of pigeons occurred as early as 10,000 years ago',
+        author: 'Krasimira Atanasova.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Feral_pigeon_%28Columba_livia_domestica%29%2C_2017-05-27.jpg/330px-Feral_pigeon_%28Columba_livia_domestica%29%2C_2017-05-27.jpg'
+    },
+]
+
+export {data};
