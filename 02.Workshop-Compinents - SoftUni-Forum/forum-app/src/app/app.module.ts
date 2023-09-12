ffe51f1ab9e7baf70a701316e57ec3c0c76e7c6a
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-12 20:17:34 +0300

    Update app.module.ts

--- 02.Workshop-Compinents - SoftUni-Forum/forum-app/src/app/app.module.ts
+++ 02.Workshop-Compinents - SoftUni-Forum/forum-app/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    CoreModule,
+    HttpClientModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
