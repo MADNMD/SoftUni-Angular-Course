a51d9cb17ee61388aa08b7a43744722ec8d9ff45
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-14 19:14:45 +0300

    Update core.module.ts

--- 02.Workshop-Compinents - SoftUni-Forum/SoftUni-Forum/forum-app/src/app/core/core.module.ts
+++ 02.Workshop-Compinents - SoftUni-Forum/SoftUni-Forum/forum-app/src/app/core/core.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
   ]
 })
 export class CoreModule { }
