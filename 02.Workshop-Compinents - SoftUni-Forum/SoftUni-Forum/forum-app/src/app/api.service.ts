fe3369fa5df2dc2921634505c387af309494e25a
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-14 19:08:06 +0300

    Update api.service.ts

--- 02.Workshop-Compinents - SoftUni-Forum/SoftUni-Forum/forum-app/src/app/api.service.ts
+++ 02.Workshop-Compinents - SoftUni-Forum/SoftUni-Forum/forum-app/src/app/api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environments.developments'
+import { Theme } from './interfaces/theme';
+import { Post } from './interfaces/post';
+
+const appUlr = environment.appUrl;
 
 @Injectable({
   providedIn: 'root'
 })
+
+//Трябва в app.module.ts да си добавим HttpClientModule - import { HttpClientModule } from '@angular/common/http';
+// Трябва и да си създадем environments къде да си сложим URL към който ще правим заявките;
+
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getThemes() {
+    return this.http.get<Theme[]>(`${appUlr}/themes`);
+  }
+
+  getPosts(limit?: number) {
+    return this.http.get<Post[]>(`${appUlr}/posts${limit ? `?limit=${limit}` : ''}`);
+  }
 }
