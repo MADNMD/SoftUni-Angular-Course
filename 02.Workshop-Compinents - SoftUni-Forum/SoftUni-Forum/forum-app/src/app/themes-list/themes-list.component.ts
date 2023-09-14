de7a37388a1f9b5f1308240c044f08569c8783f5
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-14 19:12:21 +0300

    Update themes-list.component.ts

--- 02.Workshop-Compinents - SoftUni-Forum/SoftUni-Forum/forum-app/src/app/themes-list/themes-list.component.ts
+++ 02.Workshop-Compinents - SoftUni-Forum/SoftUni-Forum/forum-app/src/app/themes-list/themes-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../interfaces/theme';
 
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
   styleUrls: ['./themes-list.component.css']
 })
-export class ThemesListComponent {
+export class ThemesListComponent implements OnInit {
 
+  themesList: Theme[] = [];
+  isLoading: boolean = true;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+
+    this.apiService.getThemes().subscribe({ // Вариант в който хващаме ако има грешка;
+      next: (themes) => {
+        this.themesList = themes;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(`Error ${err}`);
+        
+      }
+    });
+    // this.apiService.getThemes().subscribe(themes => {  // По лесен вариант без хващане на грешка
+    //   this.themesList = themes; 
+    //   // console.log(themes);
+    // });
+  }
 }
