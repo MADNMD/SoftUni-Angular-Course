79a5acc49ec704405f5df24c1a084bdf0a748339
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-17 19:18:05 +0300

    Create post.ts

---
+++ 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/interfaces/post.ts
@@ -0,0 +1,15 @@
+//Когато ги създадем ги слагаме в api.service.ts --- get<Post[]>
+
+import { User } from './userId';
+import { Theme } from './theme';
+
+export interface Post {
+    likes: string[],
+    _id: string,
+    text: string,
+    userId: User,
+    themeId: Theme,
+    created_at: string,
+    updatedAt: string,
+    __v: number,
+}
