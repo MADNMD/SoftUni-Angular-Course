ab8892c08a16e88a2198a67799ce27a71ba3d0ad
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-18 17:40:50 +0300

    Update user.service.ts

--- 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/user/user.service.ts
+++ 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/user/user.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  USER_KEY = '[user]';
+
+  user: User | undefined;
+
+  get isLogged() {
+    return !!this.user;
+  }
+
+  constructor() {
+
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+
+  login(): void {
+    this.user = {
+      email: 'misho-donchev@gmail.com',
+      firstName: 'Mihail',
+    }
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
+
 }
