72645d85bcf7f0a47cb7a678a0ef6a7033a455c5
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-19 20:19:10 +0300

    Update posts-list.component.ts

--- 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/posts-list/posts-list.component.ts
+++ 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/posts-list/posts-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Post } from '../interfaces/post';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
-export class PostsListComponent {
+export class PostsListComponent implements OnInit {
+
+  postsList: Post[] = [];
+  isLoading: boolean = true;
+  // thereAreNoPosts: boolean = false; // Това го правим ако нямаме постове да се изпише
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+
+    this.apiService.getPosts(5).subscribe({   // Вариант в който хващаме ако има грешка;
+      next: (posts) => {
+        this.postsList = posts;
+
+        // if(this.postsList.length === 0){
+        //   this.thereAreNoPosts = true;   //Това го правим ако нямаме постове да се изпише
+        // }
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(`Error ${err}`);
+      }
+    })
+
+    // this.apiService.getPosts(5).subscribe(posts => {   // По лесен вариант без хващане на грешка
+    //   this.postsList = posts;
+    //   console.log(posts);
+    // })
+  }
 
 }
