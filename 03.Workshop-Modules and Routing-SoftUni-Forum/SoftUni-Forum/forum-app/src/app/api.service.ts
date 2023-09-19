79a3559509b6b25b32bf8be95842a0d8ef31402f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-19 20:20:18 +0300

    Update api.service.ts

--- 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/api.service.ts
+++ 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/api.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environments.developments';
+import { Theme } from './interfaces/theme';
+import { Post } from './interfaces/post';
+
+const appUlr = environment.appUrl;
 
 @Injectable({
   providedIn: 'root'
 })
+
+//Трябва в app.module.ts да си добавим HttpClientModule - import { HttpClientModule } from '@angular/common/http';
+// Трябва и да си създадем environments къде да си сложим URL към който ще правим заявките;
+
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTheme(id: string) {
+    return this.http.get<Theme>(`${appUlr}/themes/${id}`);
+  }
+
+  getThemes() {
+    return this.http.get<Theme[]>(`${appUlr}/themes`);
+  }
+
+  getPosts(limit?: number) {
+    return this.http.get<Post[]>(`${appUlr}/posts${limit ? `?limit=${limit}` : ''}`);
+  }
+
+  createTheme(name: string, text: string) {
+    return this.http.post<Post>(`${appUlr}/themes`, { themeName: name, postText: text });
+  }
 }
