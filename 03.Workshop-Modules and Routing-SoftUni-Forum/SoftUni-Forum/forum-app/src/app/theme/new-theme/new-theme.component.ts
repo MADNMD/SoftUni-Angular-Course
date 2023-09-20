40e59005b8df275f5d7e167f2202fdd9bb981f8a
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-20 17:26:51 +0300

    Update new-theme.component.ts

--- 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/theme/new-theme/new-theme.component.ts
+++ 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/theme/new-theme/new-theme.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-new-theme',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class NewThemeComponent {
 
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService, private router: Router) { }
+
+  createThemeForm: FormGroup = this.formBuilder.group({
+
+    themeName: new FormControl('', []),
+    postText: new FormControl('', []),
+
+  });
+
+  createTheme(): void {
+
+    const {themeName, postText} = this.createThemeForm.value;
+    const token = localStorage.getItem('[user]');
+    const headers = new Headers({ 'Authorization': '{email: "misho-donchev@gmail.com", firstName: "Mihail"}' })
+    this.apiService.createTheme(themeName, postText).subscribe(() => {
+     this.router.navigateByUrl('/themes');
+    })
+  }
 }
