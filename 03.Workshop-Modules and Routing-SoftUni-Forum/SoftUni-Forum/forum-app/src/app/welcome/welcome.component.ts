95446cb5dbb6eef3c12764d46c97f208a85e8902
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-21 18:04:14 +0300

    Update welcome.component.ts

--- 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/welcome/welcome.component.ts
+++ 03.Workshop-Modules and Routing-SoftUni-Forum/SoftUni-Forum/forum-app/src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+
+  constructor(private userService: UserService) { }
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
 
 }
