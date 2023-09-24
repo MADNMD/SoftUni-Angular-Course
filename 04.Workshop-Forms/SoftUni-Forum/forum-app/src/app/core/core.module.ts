5cb290e8b71112992040991d4cf3ff8c4c898ee4
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-24 19:09:51 +0300

    create page-not-found component

--- 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/core/core.module.ts
+++ 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     CommonModule
