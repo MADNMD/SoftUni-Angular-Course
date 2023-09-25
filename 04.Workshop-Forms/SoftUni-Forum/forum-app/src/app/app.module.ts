edae0671c2537c0d5977aa1f1f8dcea43cdce42f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-25 17:36:13 +0300

    create themes-list component

--- 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/app.module.ts
+++ 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MainComponent,
-    PostsListComponent
+    PostsListComponent,
+    ThemesListComponent
   ],
   imports: [
     BrowserModule,
