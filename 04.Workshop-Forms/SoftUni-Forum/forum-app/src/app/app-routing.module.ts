0d750ec0726b799a9f331761ee443cd34017667c
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-26 20:00:32 +0300

    Update app-routing.module.ts

--- 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/app-routing.module.ts
+++ 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
