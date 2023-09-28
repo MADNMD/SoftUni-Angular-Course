e75fa250508362dfa8689aed9f0c0a3fe02c3fe1
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-28 20:13:07 +0300

    Update profile.component.ts

--- 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/user/profile/profile.component.ts
+++ 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/user/profile/profile.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
+
+interface Profile {
+  username: string,
+  email: string,
+  tel: string,
+}
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +15,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  editProfileForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    tel: ['', []],
+  })
+
+  isEditMode: boolean = false;
+
+  profileDetails: Profile = {
+    username: 'Mihso',
+    email: 'misho123@gmail.com',
+    tel: '12345666'
+  }
+
+  toggledEditMode(): void {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  editProfile(): void {
+
+    this.profileDetails = {...this.editProfileForm.value} as Profile;
+
+    this.toggledEditMode();
+  }
 
 }
