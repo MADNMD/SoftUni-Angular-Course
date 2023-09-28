8fe8084fe44d4fc9a761fe466d87d0fad2e10350
Mihail Donchev <mihaildonchev88@abv.bg>
2023-09-28 20:15:40 +0300

    Update register.component.ts

--- 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/user/register/register.component.ts
+++ 04.Workshop-Forms/SoftUni-Forum/forum-app/src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
+import { matchPasswordValidators } from 'src/app/shared/validators/match-password-validators';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  registerForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['', [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    tel: [''],
+    passwordGroup: this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rePassword: ['', [Validators.required]],
+    },
+      {
+        validators: [matchPasswordValidators('password', 'rePassword')]
+      })
+  })
+
+  register(): void {
+    if(this.registerForm.invalid){
+      return;
+    }
+  }
 
 }
