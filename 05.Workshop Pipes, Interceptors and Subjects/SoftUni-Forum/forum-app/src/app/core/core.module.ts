be4ec496b6fcbf93bcdd8346635baa77918dcc45
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-03 23:38:20 +0300

    Update core.module.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/core/core.module.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/core/core.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ErrorComponent } from './error/error.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorComponent } from './error/error.component';
 
 
 
 @NgModule({
   declarations: [
-    ErrorComponent,
-    FooterComponent,
     HeaderComponent,
-    PageNotFoundComponent
+    FooterComponent,
+    PageNotFoundComponent,
+    ErrorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
   ]
 })
 export class CoreModule { }
