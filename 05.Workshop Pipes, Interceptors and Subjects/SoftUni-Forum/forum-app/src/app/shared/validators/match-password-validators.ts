09b03d48d6e6765fc0c582e975e2aadaeefb41d2
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-04 23:09:28 +0300

    Create match-password-validators.ts

---
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/shared/validators/match-password-validators.ts
@@ -0,0 +1,16 @@
+import { FormGroup, ValidatorFn } from "@angular/forms";
+
+export function matchPasswordValidators(
+    passwordCtrOne: string,
+    passwordCtrTwo: string)
+    : ValidatorFn {
+    return (control) => {
+        const group = control as FormGroup;
+        const pass1 = group.get(passwordCtrOne);
+        const pass2 = group.get(passwordCtrTwo);
+
+        return pass1?.value === pass2?.value
+            ? null
+            : { matchPasswordValidators: true }
+    }
+}
