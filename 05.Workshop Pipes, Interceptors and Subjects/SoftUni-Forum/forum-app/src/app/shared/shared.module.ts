ef2126a95a0b06dc83253d658e8d078353eaf7db
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-04 23:15:24 +0300

    create elapsed-time pipe

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/shared/shared.module.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { EmailDirective } from './validators/email.directive';
+import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    EmailDirective
+    EmailDirective,
+    ElapsedTimePipe
   ],
   imports: [
     CommonModule
