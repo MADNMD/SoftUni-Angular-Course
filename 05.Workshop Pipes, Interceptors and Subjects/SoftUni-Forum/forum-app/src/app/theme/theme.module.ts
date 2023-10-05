f0f36d2acdeca838e4e0c1eec9a79e392c64eec6
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-05 19:48:07 +0300

    create current-theme component

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/theme/theme.module.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/theme/theme.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CurrentThemeComponent
+  ],
   imports: [
     CommonModule
   ]
