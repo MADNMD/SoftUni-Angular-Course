27259644494c0b8393df2dcb415b2a23377779b7
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-05 19:55:24 +0300

    Update user.module.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/user/user.module.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/user/user.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserRoutingModule } from './user-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    ProfileComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule,
+    FormsModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ]
 })
 export class UserModule { }
