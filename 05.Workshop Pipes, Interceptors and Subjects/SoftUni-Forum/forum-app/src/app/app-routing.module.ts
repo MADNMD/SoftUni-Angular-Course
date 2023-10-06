c66eb77bcc39df847887372f6f2b6cfa921a555c
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-06 18:36:03 +0300

    Update app-routing.module.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/app-routing.module.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { ErrorComponent } from './core/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    	path: 'auth',
+      loadChildren: () => import('./user/user.module').then(module => module.UserModule),
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
