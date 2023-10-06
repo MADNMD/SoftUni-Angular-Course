fdaefa1dde852cb66633d73e8c7252d3ba6b82aa
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-06 18:33:38 +0300

    Update error.component.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/core/error/error.component.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/core/error/error.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ErrorService } from './error.service';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit{
+
+  errorMsg = '';
+
+  constructor(private errorService: ErrorService) {}
+  
+  apiError$ = this.errorService.apiError$$.asObservable();
+
+  ngOnInit(): void {
+    this.apiError$.subscribe((error: any) => {
+      this.errorMsg = error.message;
+    })
+  }
 
 }
