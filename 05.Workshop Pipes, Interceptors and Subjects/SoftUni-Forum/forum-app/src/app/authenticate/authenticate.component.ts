5866b55506cb70b8e67f27bb89e358a645025fa4
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-07 19:36:07 +0300

    Update authenticate.component.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/authenticate/authenticate.component.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/authenticate/authenticate.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
   styleUrls: ['./authenticate.component.css']
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+
+  isAuthenticated = true;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe({
+      next: () => { this.isAuthenticated = false },
+      error: () => { this.isAuthenticated = false },
+      complete: () => { this.isAuthenticated = false }
+    })
+  }
 
 }
