9fd540808e6ae9c1431840d3526e8c10f0819225
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-08 23:00:58 +0300

    Update current-theme.component.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/theme/current-theme/current-theme.component.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/theme/current-theme/current-theme.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Theme } from 'src/app/interfaces/theme';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-current-theme',
   templateUrl: './current-theme.component.html',
   styleUrls: ['./current-theme.component.css']
 })
-export class CurrentThemeComponent {
+export class CurrentThemeComponent implements OnInit{
+
+  theme: Theme | undefined;
+
+  constructor(private apiService: ApiService, private activatedRouter: ActivatedRoute, private userService: UserService) { }
+
+  get isLogged(): boolean{
+      return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.fetchTheme()
+  }
+
+  fetchTheme(): void {
+
+    const themeId = this.activatedRouter.snapshot.params['themeId'];
+
+    this.apiService.getTheme(themeId).subscribe(currentTheme => {
+      this.theme = currentTheme;
+      console.log(this.theme);
+    })
+  }
 
 }
