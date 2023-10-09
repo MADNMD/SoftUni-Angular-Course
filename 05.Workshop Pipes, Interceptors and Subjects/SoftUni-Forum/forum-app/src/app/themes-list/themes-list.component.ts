d3e1d43421c2665b8122e12ba56e5680e534bdea
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-09 23:09:54 +0300

    Update themes-list.component.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/themes-list/themes-list.component.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/themes-list/themes-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../interfaces/theme';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
   styleUrls: ['./themes-list.component.css']
 })
-export class ThemesListComponent {
+export class ThemesListComponent implements OnInit {
 
+  themesList: Theme[] = [];
+  isLoading: boolean = true;
+
+  constructor(private apiService: ApiService, private userService: UserService) { }
+
+  get isLogged(): boolean { //това го правим за да показваме зелените бутончета subscribe
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+
+    this.apiService.getThemes().subscribe({ // Вариант в който хващаме ако има грешка;
+      next: (themes) => {
+        this.themesList = themes;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(`Error ${err}`);
+
+      }
+    });
+    // this.apiService.getThemes().subscribe(themes => {  // По лесен вариант без хващане на грешка
+    //   this.themesList = themes; 
+    //   // console.log(themes);
+    // });
+  }
 }
