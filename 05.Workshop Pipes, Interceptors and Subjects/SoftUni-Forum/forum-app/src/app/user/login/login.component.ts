a24e7ee3ffd2599876ecd13a4a89f817b7195e52
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-09 23:11:12 +0300

    Update login.component.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/user/login/login.component.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  appEmailDomains = DEFAULT_EMAIL_DOMAINS; // това го подаваме в HTML
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  login(loginForm: NgForm): void {
+
+    if (loginForm.invalid) {
+      return;
+    }
+
+    const { email, password } = loginForm.value;
+
+    this.userService.login(email, password).subscribe(() => {
+      this.router.navigateByUrl('/themes');
+    })
+  }
 
 }
