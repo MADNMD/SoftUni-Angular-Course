3ea36ac5a71414776ee359e1e44b5bac44b45e6d
Mihail Donchev <mihaildonchev88@abv.bg>
2023-10-09 23:13:25 +0300

    Update register.component.ts

--- 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/user/register/register.component.ts
+++ 05.Workshop Pipes, Interceptors and Subjects/SoftUni-Forum/forum-app/src/app/user/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
+import { matchPasswordValidators } from 'src/app/shared/validators/match-password-validators';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +12,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router) { }
+
+  registerForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['', [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    tel: [''],
+    passwordGroup: this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rePassword: ['', [Validators.required]],
+    },
+      {
+        validators: [matchPasswordValidators('password', 'rePassword')]
+      })
+  })
+
+  register() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const { username, email, tel, passwordGroup: { password, rePassword } = {} } = this.registerForm.value;
+
+    this.userService.register(username!, email!,  password!, rePassword!, tel!).subscribe(() => {
+      this.router.navigateByUrl('/themes')
+    })
+
+  }
 
 }
